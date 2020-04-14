494219d1381e272d99956645bee1c88cd50f9030
mauropasse <mauropasse@hotmail.com>
2020-04-14 21:00:27 +0100

    Update build-test.sh

--- .github/workflows/build-test.sh
+++ .github/workflows/build-test.sh
@@ -7,37 +7,44 @@ echo "THIS_DIR: $THIS_DIR"
 # Download our perf framework
 git clone https://github.com/irobot-ros/ros2-performance
 
+echo "Files here: (I should see ros2-performance)"
+ls
+
 # Static Executor with waitable
 cd $THIS_DIR/ros2-performance/cross-compiling
 bash get_ros2_sources.sh master
 
-mv ros2_cc_ws $THIS_DIR
+echo "I'm in: $PWD. There are:"
 
 ls $THIS_DIR
 
 cd $THIS_DIR/ros2_cc_ws/src/ros2
 
-# Download our perf framework
-cp -r $THIS_DIR/ros2-performance/performances .
+echo "I'm in: $PWD. There are:"
 
-# Use my own RCLCPP
-rm -rf rclcpp
-git clone https://github.com/mauropasse/rclcpp.git
+ls $THIS_DIR
 
-# Ignore packages
-# cd $THIS_DIR/ros2-performance/cross-compiling
-# source env.sh raspbian
-# bash ignore_pkgs.sh $THIS_DIR/ros2_cc_ws/ master
+# # Download our perf framework
+# cp -r $THIS_DIR/ros2-performance/performances .
 
-# Compile
-cd $THIS_DIR/ros2_cc_ws
+# # Use my own RCLCPP
+# rm -rf rclcpp
+# git clone https://github.com/mauropasse/rclcpp.git
 
-colcon \
-    build \
-    --merge-install \
-    --cmake-force-configure \
-    --cmake-args \
-    -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON \
-    -DTHIRDPARTY=ON \
-    -DBUILD_TESTING:BOOL=OFF
+# # Ignore packages
+# # cd $THIS_DIR/ros2-performance/cross-compiling
+# # source env.sh raspbian
+# # bash ignore_pkgs.sh $THIS_DIR/ros2_cc_ws/ master
+
+# # Compile
+# cd $THIS_DIR/ros2_cc_ws
+
+# colcon \
+#     build \
+#     --merge-install \
+#     --cmake-force-configure \
+#     --cmake-args \
+#     -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON \
+#     -DTHIRDPARTY=ON \
+#     -DBUILD_TESTING:BOOL=OFF
 
