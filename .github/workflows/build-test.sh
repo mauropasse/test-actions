effeff85d0a0a5d2b3c724ab1605a87edb4da7b4
mauropasse <mauropasse@hotmail.com>
2020-04-14 15:30:39 +0100

    Update build-test.sh

--- .github/workflows/build-test.sh
+++ .github/workflows/build-test.sh
@@ -4,7 +4,7 @@ ls
 THIS_DIR=$PWD
 
 # Static Executor with waitable
-cd ros2-performance/cross-compiling
+cd $THIS_DIR/ros2-performance/cross-compiling
 bash get_ros2_sources.sh master
 
 cd $THIS_DIR/ros2_cc_ws/src/ros2
@@ -17,7 +17,7 @@ rm -rf rclcpp
 git clone https://github.com/mauropasse/rclcpp.git
 
 # Ignore packages
-cd ~/ROS/doc/cross-compiling-irobot-platforms/ros2-performance/cross-compiling
+cd $THIS_DIR/ros2-performance/cross-compiling
 source env.sh raspbian
 bash ignore_pkgs.sh $THIS_DIR/ros2_cc_ws/ master
 
