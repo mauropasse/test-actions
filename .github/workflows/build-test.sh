e6f8aa0670285671c054c1acabaee18bdaa07c52
mauropasse <mauropasse@hotmail.com>
2020-04-15 19:12:05 +0100

    Update build-test.sh

--- .github/workflows/build-test.sh
+++ .github/workflows/build-test.sh
@@ -3,49 +3,31 @@
 THIS_DIR=$PWD
 HOME=/home/runner
 
-echo "THIS_DIR: $THIS_DIR"
-
 # Download our perf framework
 git clone https://github.com/irobot-ros/ros2-performance
-
-echo "Files here: (I should see ros2-performance)"
-ls
 
 # Static Executor with waitable
 cd $THIS_DIR/ros2-performance/cross-compiling
 bash get_ros2_sources.sh master
 
-echo "I'm in: $PWD. ls:"
-
-ls
-
 cd $HOME/ros2_cc_ws/src/ros2
 
-echo "I'm in: $PWD. ls:"
+# Download our perf framework
+cp -r $THIS_DIR/ros2-performance/performances .
 
-ls
+# Use my own RCLCPP
+rm -rf rclcpp
+git clone https://github.com/mauropasse/rclcpp.git -b mauro/master
 
-# # Download our perf framework
-# cp -r $THIS_DIR/ros2-performance/performances .
+# Compile
+cd $HOME/ros2_cc_ws
 
-# # Use my own RCLCPP
-# rm -rf rclcpp
-# git clone https://github.com/mauropasse/rclcpp.git
+colcon build --symlink-install --packages-up-to benchmark
 
-# # Ignore packages
-# # cd $THIS_DIR/ros2-performance/cross-compiling
-# # source env.sh raspbian
-# # bash ignore_pkgs.sh $THIS_DIR/ros2_cc_ws/ master
+source install/setup.bash
+cd install/benchmark/lib/benchmark
 
-# # Compile
-# cd $THIS_DIR/ros2_cc_ws
+./benchmark topology/cedar.json 
 
-# colcon \
-#     build \
-#     --merge-install \
-#     --cmake-force-configure \
-#     --cmake-args \
-#     -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON \
-#     -DTHIRDPARTY=ON \
-#     -DBUILD_TESTING:BOOL=OFF
+cat cedar_log/resources.txt
 
